2155cb9d19f397c8bfe099ee173455d544564169
Daviid24x <estebanbernardodavid1@gmail.com>
2023-12-11 16:19:01 +0100

    Redo

--- practica2/src/app.js
+++ practica2/src/app.js
@@ -14,7 +14,7 @@ app.engine('html', mustacheExpress(), "html");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public'));
 
 app.use('/', boardRouter);
 
